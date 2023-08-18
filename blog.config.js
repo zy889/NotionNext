d4e1fb8b1aaf2d5f59eb0b5622e997093268806d
zy889 <68155003+zy889@users.noreply.github.com>
2023-08-18 13:33:20 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '趣集客', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'UJK.TOP', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || 'QUJK.TOP', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://qujk.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '趣集客,资源,分享,趣站,博客', // 网站关键词 英文逗号隔开
 
