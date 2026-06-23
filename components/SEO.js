863649e874af71f7c8c994d50fed8d5e8db93484
zy889 <68155003+zy889@users.noreply.github.com>
2026-06-23 13:02:26 +0800

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -71,6 +71,13 @@ const SEO = props => {
     NOTION_CONFIG
   )
 
+  // 搜狗站点验证
+  const SEO_SOGOU_SITE_VERIFICATION = siteConfig(
+    'SEO_SOGOU_SITE_VERIFICATION',
+    null,
+    NOTION_CONFIG
+  )
+
   const BLOG_FAVICON = siteConfig('BLOG_FAVICON', null, NOTION_CONFIG)
 
   const COMMENT_WEBMENTION_ENABLE = siteConfig(
@@ -126,6 +133,12 @@ const SEO = props => {
         <meta
           name='baidu-site-verification'
           content={SEO_BAIDU_SITE_VERIFICATION}
+        />
+      )}
+      {SEO_SOGOU_SITE_VERIFICATION && (
+        <meta
+          name='sogou_site_verification'
+          content={SEO_SOGOU_SITE_VERIFICATION}
         />
       )}
 
